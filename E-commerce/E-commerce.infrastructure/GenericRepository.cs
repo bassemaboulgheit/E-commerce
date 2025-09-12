39af2000b5431ef8174e4706648a7af5bac0297c
Mohamed Ayman <74455634+Eng-Mohamed102@users.noreply.github.com>
2025-09-12 21:18:22 +0300

    Add files via upload

---
+++ E-commerce/E-commerce.infrastructure/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using E_commerce.Application.Contracts;
+using E_commerce.Context;
+using E_commerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.infrastructure
+{
+    public class GenericRepository<T, TId> : IGenericRepository<T, TId> where T : BaseModels<TId>
+    {
+        MyContext MyEcommerceContext;
+        DbSet<T> _dbset;
+
+        public GenericRepository(MyContext Context)
+        {
+            MyEcommerceContext = Context;
+            _dbset = MyEcommerceContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbset;
+        }
+
+        public T GetById(TId pk)
+        {
+            return _dbset.Find(pk);
+        }
+        //public T GetByName(T name)
+        //{
+        //    return _dbset.FirstOrDefault();
+        //}
+
+        public void create(T entity)
+        {
+            //MyEcommerceContext.Categories.Add(category);
+            _dbset.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbset.Update(entity);
+        }
+
+        public void delete(T entity)
+        {
+            //MyEcommerceContext.Categories.Remove(category);
+            _dbset.Remove(entity);
+        }
+
+        public int Save()
+        {
+            return MyEcommerceContext.SaveChanges();
+        }
+    }
+}
---
+++ E-commerce/E-commerce.infrastructure/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using E_commerce.Application.Contracts;
+using E_commerce.Context;
+using E_commerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.infrastructure
+{
+
+    public class UserRepository : IUserRepository
+    {
+        MyContext _context;
+        public UserRepository(MyContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return _context.Users.AsQueryable();
+        }
+        public async Task<User> LoginAsync(string username, string password)
+        {
+            var users = GetAll();
+            return await users.AsNoTracking().SingleOrDefaultAsync(u => (u.Email == username || u.Name == username) && u.Password == password);
+        }
+
+        public async Task Register(User user)
+        {
+            await _context.Users.AddAsync(user);
+                
+        }
+
+        public void saveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task<bool> ExistsByEmailAsync(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+    }
+}
